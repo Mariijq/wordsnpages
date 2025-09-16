f7616e31cba44391fdf1db12e562616b3a025355
marija130059 <ms30059@seeu.edu.mk>
2025-09-17 01:42:56 +0200

    first commit

---
+++ wordsnpages/Repositories/Implementations/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using wordsnpages.Models;
+using wordsnpages.Repositories.Interfaces;
+
+namespace wordsnpages.Repositories.Implementations
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository 
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) :base (db)
+        {
+            _db= db;
+        }
+
+        public void Update (ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Update(applicationUser);
+        }
+
+    }
+}
---
+++ wordsnpages/Repositories/Interfaces/IApplicationUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using wordsnpages.Models;
+
+namespace wordsnpages.Repositories.Interfaces
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+        public void Update(ApplicationUser applicationUser);
+
+    }
+}
